aa58ff0c4d4d9271da470ff5dcbb287a0c935f96
Niels Viberg <nieso@nuuday.dk>
2020-02-17 16:27:30 +0100

    Cleaned up, added typescript and styled components

---
+++ src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { DeleteMe } from "./styled";
+
+function App() {
+  return (
+    <div className="App">
+      <DeleteMe>Implement the menu instead of me 🦄</DeleteMe>
+    </div>
+  );
+}
+
+export default App;
