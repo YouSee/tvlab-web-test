8224e77f4c0563e7343d1ba18a04b6b9299bae1d
Niels Viberg <nieso@nuuday.dk>
2020-02-18 08:50:21 +0100

    Added design, necessary icons and removed uncessary files

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,7 +4,12 @@ import { DeleteMe } from "./styled";
 function App() {
   return (
     <div className="App">
-      <DeleteMe>Implement the menu instead of me 🦄</DeleteMe>
+      <DeleteMe>
+        Implement the menu instead of me
+        <span role="img" aria-label="unicorn icon">
+          🦄
+        </span>
+      </DeleteMe>
     </div>
   );
 }
